ed8c28559cdaf9192ddcd735303a828515c769ad
JackSmellyDog <shaposhnikandriy@gmail.com>
2024-06-02 16:43:25 +0300

    ref: None-web application mode settings to the configuration

--- src/main/java/me/shaposhnik/monocli/MonoCliApplication.java
+++ src/main/java/me/shaposhnik/monocli/MonoCliApplication.java
@@ -1,8 +1,7 @@
 package me.shaposhnik.monocli;
 
-import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
@@ -10,10 +9,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 public class MonoCliApplication {
 
   public static void main(String[] args) {
-    // todo: to configuration
-    new SpringApplicationBuilder(MonoCliApplication.class)
-        .web(WebApplicationType.NONE)
-        .run(args);
+    System.exit(SpringApplication.exit(SpringApplication.run(MonoCliApplication.class, args)));
   }
 
 }
